8d2dc21eb53e5fe338dc65c26573676fb059e477
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-05-16 03:06:38 +0000

    create movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,17 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def create
+    d = Movie.new
+    d.title = params.fetch("title_field")
+    d.year = params.fetch("year_field")
+    d.duration = params.fetch("duration_field")
+    d.description = params.fetch("description_field")
+    d.image = params.fetch("image_field")
+    d.director_id = params.fetch("director_field")
+    d.save
+
+    redirect_to("/movies")
+  end
 end
